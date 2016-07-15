00b64ed668c4ff279d76cb8bacb95ac105e2be7d
Yashwanth Reddy <yashwanthvenati@gmail.com>
2016-07-15 20:56:09 +0530

    Un-necessary imports removed

--- analyse/views.py
+++ analyse/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 from django.http import HttpResponseRedirect
-from .forms import ScrapeForm
 
 from .models import Amazon_Analyse
 # Create your views here.
@@ -10,13 +9,11 @@ def analyse_data(request):
 	if request.method == 'GET':
 		q = Amazon_Analyse()
 		data= q.analyse_class()
-		form = ScrapeForm()
-		return  render(request, 'result.html', {'data': data,'form':form})
+		return  render(request, 'result.html', {'data': data})
 
 def analyse_data_list_all(request):
 	if request.method == 'GET':
 		q = Amazon_Analyse()
 		data = q.analyse_class()
-		form = ScrapeForm()
 		return  render(request, 'index.html', {'data': data})
 
