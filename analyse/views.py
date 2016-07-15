d8587d349a7b1fed964e07f38be3ec980f33b41a
Yashwanth Reddy <yashwanthvenati@gmail.com>
2016-07-15 23:15:50 +0530

    Comments display basic impl

--- analyse/views.py
+++ analyse/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .models import Amazon_Analyse
+
+import json
 # Create your views here.
 
 def analyse_data(request):
 	if request.method == 'GET':
 		q = Amazon_Analyse()
 		data= q.analyse_class()
-		return  render(request, 'result.html', {'data': data})
+
+		#for testing display
+		dictionary = {
+				'battery' : ['bad battery','good battery','worst battery'],
+				'camera' : ['badcamera','goodcamera','worstcamera'],
+				'look' : ['bad','good','worst'],
+				'design' : ['bad','good','worst']
+		}
+		return  render(request, 'result.html', {'data': data, 'comments': json.dumps(dictionary)})
 
 def analyse_data_list_all(request):
 	if request.method == 'GET':
